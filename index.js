17eb684a6a4f4ebe537a3fc697c6909cbe0e2233
WilliamsiQ <68313949+WilliamsiQ@users.noreply.github.com>
2022-06-22 14:45:12 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+let name = 'Willihots';
+let height = 6;
+let country = 'Nigeria';
+
+const intro =()=>{
+    console.log(`my name is ${name} i am ${height}ft tall, i am from ${country}`);
+
+}
+
+intro()
